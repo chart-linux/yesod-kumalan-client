2bfd8b027ebda4d30cb19ba03b7c27d4dfefc002
Y.Sangenya <longinus.eva@gmail.com>
2016-04-27 23:02:17 +0900

    Device登録の際に、ユーザーが自動で選択されるようにした

--- Handler/Device.hs
+++ Handler/Device.hs
@@ -15,14 +15,15 @@ getDevicesR = do
 getDeviceCreateR :: Handler Html
 getDeviceCreateR = do
   userIdStringMaybe <- lookupGetParam "user_id"
-  users <- runDB $ selectList [] []
-  (formWidget, formEnctype) <- generateFormPost $ deviceForm (readMay =<< userIdStringMaybe) users
-  defaultLayout $(widgetFile "device/create")
+  case (readMay =<< userIdStringMaybe) of
+    Just userId -> do
+      (formWidget, formEnctype) <- generateFormPost $ deviceForm (Just userId)
+      defaultLayout $(widgetFile "device/create")
+    Nothing -> notFound
 
 postDeviceCreateR :: Handler Html
 postDeviceCreateR = do
-  users <- runDB $ selectList [] []
-  ((result, formWidget), formEnctype) <- runFormPost $ deviceForm Nothing users
+  ((result, formWidget), formEnctype) <- runFormPost $ deviceForm Nothing
   case result of
     FormSuccess device -> do
       _ <- runDB $ insert device
@@ -35,8 +36,8 @@ getDeviceDeleteR deviceId = do
   _ <- runDB $ delete deviceId
   redirect (DevicesR, [("user_id", pack . show . deviceUserId $ device)])
 
-deviceForm :: Maybe UserId -> [Entity User] -> Html -> MForm Handler (FormResult Device, Widget)
-deviceForm userIdMaybe users = renderBootstrap3 (BootstrapHorizontalForm (ColSm 0) (ColSm 2) (ColSm 0) (ColSm 3)) $ Device
-  <$> areq (selectFieldList [(userName user, userid) | Entity userid user <- users ]) (bfs ("持ち主" :: Text)) userIdMaybe
+deviceForm :: Maybe UserId -> Html -> MForm Handler (FormResult Device, Widget)
+deviceForm userIdMaybe = renderBootstrap3 (BootstrapHorizontalForm (ColSm 0) (ColSm 2) (ColSm 0) (ColSm 3)) $ Device
+  <$> areq hiddenField "" userIdMaybe
   <*> areq textField (bfs ("MACアドレス" :: Text)) Nothing
   <*  bootstrapSubmit (BootstrapSubmit ("登録" :: Text) "btn-primary" [("attribute-name","attribute-value")])
