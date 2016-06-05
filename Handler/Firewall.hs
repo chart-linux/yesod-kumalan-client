fe0b2a328c8898ad188e14a414b9173f5a2ebe14
Yusuke Sangenya <longinus.eva@gmail.com>
2016-06-06 02:22:22 +0900

    Forwardingだけフィルタするように。また、callProcessで書き換えた。また、rejectも書き加えた。

--- Handler/Firewall.hs
+++ Handler/Firewall.hs
@@ -1,21 +1,21 @@
 module Handler.Firewall where
 
-import Import hiding (hPutStrLn)
-import System.IO.Temp
-import System.Process
+import Import hiding (writeFile)
 import System.IO
+import System.Process
 
 getFirewallR :: Handler Html
 getFirewallR = do
-  users <- runDB $ selectList [ UserPaid ==. True ] []
+  users <- runDB $ selectList ([ UserPaid ==. True ] ||. [ UserExempted ==. True ]) []
   _devices <- forM users $ \(Entity userid _) -> do
     runDB $ selectList [ DeviceUserId ==. userid ] []
   let devices = concat _devices
-  withSystemTempFile "firewall.user" $ \path hdl -> liftIO $ do
-    forM_ devices $ \(Entity _ device) -> do
+  liftIO $ do
+    accepts <- forM devices $ \(Entity _ device) -> do
       let mac = map (\c -> if c == '-' then ':' else c) (unpack $ deviceMacAddress device)
-      hPutStrLn hdl ( "iptables -A forwarding_lan_rule -m mac --mac-source " ++ mac ++ " -j ACCEPT" )
-    hFlush hdl
-    callCommand ("scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null " ++ path ++ " root@kumalan.dip.jp:/etc/firewall.user")
-    callCommand "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@kumalan.dip.jp /etc/init.d/firewall restart"
+      return ("iptables -A forwarding_lan_rule -m mac --mac-source " ++ mac ++ " -j ACCEPT")
+    let script = accepts ++ ["iptables -A forwarding_lan_rule -j REJECT"]
+    writeFile "/tmp/firewall.user" (unlines script)
+    callProcess "/usr/bin/scp" ["-o", "StrictHostKeyChecking=no", "-o","UserKnownHostsFile=/dev/null", "/tmp/firewall.user", "root@kumalan.dip.jp:/etc/firewall.user"]
+    callProcess "/usr/bin/ssh" ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null", "root@kumalan.dip.jp", "/etc/init.d/firewall", "restart"]
   redirect HomeR
